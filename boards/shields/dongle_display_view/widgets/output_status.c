197dc4d75d46c3920eb055dfa648eefa5691af87
Filip Lindau <filip.maxlab@gmail.com>
2026-04-23 20:20:42 +0200

    output_status

--- boards/shields/dongle_display_view/widgets/output_status.c
+++ boards/shields/dongle_display_view/widgets/output_status.c
@@ -67,7 +67,7 @@ struct output_status_state {
 
 static struct output_status_state get_state(const zmk_event_t *_eh) {
     return (struct output_status_state){
-        .selected_endpoint = zmk_endpoints_selected(),
+        .selected_endpoint = zmk_endpoint_get_selected(),
         .active_profile_index = zmk_ble_active_profile_index(),
         .active_profile_connected = zmk_ble_active_profile_is_connected(),
         .active_profile_bonded = !zmk_ble_active_profile_is_open(),
@@ -84,11 +84,25 @@ static void anim_size_cb(void * var, int32_t v) {
 }
 
 static void move_object_x(void *obj, int32_t from, int32_t to) {
-    lv_anim_start(obj, lv_anim_exec_x_cb, from, to, 200, lv_anim_path_overshoot);
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_var(&a, obj);
+    lv_anim_set_values(&a, from, to);
+    lv_anim_set_time(&a, 200);
+    lv_anim_set_exec_cb(&a, (lv_anim_exec_xcb_t)lv_obj_set_x);
+    lv_anim_set_path_cb(&a, lv_anim_path_overshoot);
+    lv_anim_start(&a);
 }
 
 static void change_size_object(void *obj, int32_t from, int32_t to) {
-    lv_anim_start(obj, anim_size_cb, from, to, 200, lv_anim_path_ease_in_out);
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_var(&a, obj);
+    lv_anim_set_values(&a, from, to);
+    lv_anim_set_time(&a, 200);
+    lv_anim_set_exec_cb(&a, (lv_anim_exec_xcb_t)anim_size_cb);
+    lv_anim_set_path_cb(&a, lv_anim_path_ease_in_out);
+    lv_anim_start(&a);
 }
 
 static void set_status_symbol(lv_obj_t *widget, struct output_status_state state) {
