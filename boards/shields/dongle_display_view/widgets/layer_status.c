4a80bdf4fa36dbacfe2333a0fcae73ce324ed1dc
Filip Lindau <filip.maxlab@gmail.com>
2026-04-25 09:44:29 +0200

    include

--- boards/shields/dongle_display_view/widgets/layer_status.c
+++ boards/shields/dongle_display_view/widgets/layer_status.c
@@ -9,7 +9,8 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
-#include <zmk/display/widgets/layer_status.h>
+//#include <zmk/display/widgets/layer_status.h>
+#include "widgets/layer_status.h"
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/event_manager.h>
 #include <zmk/endpoints.h>
