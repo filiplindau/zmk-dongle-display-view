47d895aac2ae3bcc98b151f28007687ededdc98e
Filip Lindau <filip.maxlab@gmail.com>
2026-04-26 21:31:53 +0200

    white bkg

--- boards/shields/dongle_display_view/custom_status_screen.c
+++ boards/shields/dongle_display_view/custom_status_screen.c
@@ -30,9 +30,11 @@ lv_obj_t *zmk_display_status_screen() {
 
     screen = lv_obj_create(NULL);
 
-    //lv_obj_set_style_bg_color(screen, lv_color_black(), 0);
-    lv_obj_set_style_bg_color(screen, lv_color_hex(0x333333), 0);
+    lv_obj_set_style_bg_color(screen, lv_color_white(), 0);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
+
+    lv_obj_set_style_text_color(screen, lv_color_black(), 0);
+    lv_obj_set_style_line_color(screen, lv_color_black(), 0);
 
     lv_obj_set_style_text_font(screen, &lv_font_unscii_8, 0);
     lv_obj_set_style_text_letter_space(screen, 1, 0);
